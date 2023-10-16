1d864df18235748e2e71a741663519cceeb3563e
Omkar Vijay Pitale <omkarvijaypitale@Omkars-MacBook-Pro.local>
2023-10-16 18:24:43 -0400

    Add delete schema script

---
+++ sql/delete.sql
@@ -0,0 +1,27 @@
+DROP TABLE Published_by;
+
+DROP TABLE Classified_by;
+
+DROP TABLE Written_by;
+
+DROP TABLE Subscribe;
+
+DROP TABLE Accessed_by;
+
+DROP TABLE Payments;
+
+DROP TABLE Customers;
+
+DROP TABLE Employees;
+
+DROP TABLE Authors;
+
+DROP TABLE Books;
+
+DROP TABLE PaymentTransactions;
+
+DROP TABLE Subscriptions;
+
+DROP TABLE Publishers;
+
+DROP TABLE Sections;
