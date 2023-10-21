6256891262adf54e2b4be9969132dc65bb01ed11
akshay-022 <62378087+akshay-022@users.noreply.github.com>
2023-10-21 13:52:36 -0400

    queries updated

--- sql/interesting_queries.sql
+++ sql/interesting_queries.sql
@@ -1,4 +1,4 @@
--- Query 1
+-- Query 1 How many distinct sections every customer has access to
 SELECT A.user_id,
   A.given_name,
   A.last_name,
@@ -11,6 +11,40 @@ WHERE A.user_id = W.user_id
 GROUP BY A.user_id
 ORDER BY number_of_sections DESC;
 
--- Query 2
--- Query 3
--- Query 4
+-- Query 2 Selecting all customers who bought the more expensive subscription and renewed it at least once
+SELECT A.user_id,
+  A.given_name,
+  A.last_name,
+  A.email,
+  COUNT(A.transaction_id) AS number_of_renewals
+FROM Customers A,
+  Payments P
+WHERE A.user_id = P.user_id
+  AND P.subscription_id = 2
+GROUP BY A.user_id
+HAVING COUNT(A.transaction_id) >= 2
+ORDER BY number_of_sections DESC;
+
+-- Query 3 books accessible by both subscriptions and published after 2000
+SELECT B.book_id,
+  B.book_name,
+  B.year
+FROM Books B,
+  Accessed_by A
+WHERE B.publication_year > 2000
+  AND A.book_id = B.book_id
+GROUP BY B.book_id
+HAVING COUNT(A.subscription_id) = 2;
+
+-- Query 4 All users that transacted payments in both August and September 2023
+SELECT C.user_id,
+  C.given_name,
+  C.last_name,
+  FROM Customers C,
+  PaymentTransactions T,
+  Payments P
+WHERE T.transaction_id = P.transaction_id
+  AND T.time_stamp ~ '^.*-0[89]-.*$'
+  AND P.user_id = C.user_id
+GROUP BY C.user_id
+HAVING COUNT(T.time_stamp) = 2;
