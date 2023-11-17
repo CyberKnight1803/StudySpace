be244e28db5bec59c18b6c554e2d546ce1320c06
Omkar Vijay Pitale <omkarvijaypitale@Omkars-MacBook-Pro.local>
2023-11-17 12:25:01 -0500

    Add run server details

--- server.py
+++ server.py
@@ -63,5 +63,31 @@ app.register_blueprint(author_bp)
 app.register_blueprint(book_bp)
 
 
+
+
 if __name__=="__main__":
-  app.run(debug=DEVLOPMENT_ENV) 
+  import click
+
+  @click.command()
+  @click.option('--debug', is_flag=True)
+  @click.option('--threaded', is_flag=True)
+  @click.argument('HOST', default='0.0.0.0')
+  @click.argument('PORT', default=8111, type=int)
+  def run(debug, threaded, host, port):
+    """
+    This function handles command line parameters.
+    Run the server using:
+
+        python3 server.py
+
+    Show the help text using:
+
+        python3 server.py --help
+
+    """
+
+    HOST, PORT = host, port
+    print("running on %s:%d" % (HOST, PORT))
+    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+
+  run()
