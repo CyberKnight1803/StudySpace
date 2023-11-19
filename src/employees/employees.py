0a716225e47e1b54d21e81b0786e99ddeddbd6a2
akshay-022 <62378087+akshay-022@users.noreply.github.com>
2023-11-18 22:43:37 -0500

    Added employees all (with deletion error)

--- src/employees/employees.py
+++ src/employees/employees.py
@@ -102,4 +102,91 @@ def update_profile():
   except Exception as e:
     return redirect(employee_bp.url_prefix + '/profile' + '?incorrect_details=True')
 
-  return redirect(employee_bp.url_prefix + '/profile') 
+#add publisher, remove customer
+#View customers, view authors, view publishers
+#Customers
+
+
+
+#All the views
+@employee_bp.route('/customers', methods=['GET'])
+def view_customers():
+  if 'current_user_id' not in session or session['user_type'] != 'employee':
+    return redirect(employee_bp.url_prefix + '/login')
+  cursor = g.conn.execute(text("""
+    SELECT * 
+    FROM Customers C
+  """))
+  query_res = cursor
+  if query_res is None:
+    return redirect(employee_bp.url_prefix)
+  return render_template('employees/views/customers.html', customers=query_res)
+@employee_bp.route('/publishers', methods=['GET'])
+def view_publishers():
+  if 'current_user_id' not in session or session['user_type'] != 'employee':
+    return redirect(employee_bp.url_prefix + '/login')
+  cursor = g.conn.execute(text("""
+    SELECT * 
+    FROM Publishers P
+  """))
+  query_res = cursor
+  if query_res is None:
+    return redirect(employee_bp.url_prefix)
+  return render_template('employees/views/publishers.html', publishers=query_res)
+@employee_bp.route('/authors', methods=['GET']) 
+def view_authors():
+  if 'current_user_id' not in session or session['user_type'] != 'employee':
+    return redirect(employee_bp.url_prefix + '/login')
+  cursor = g.conn.execute(text("""
+    SELECT * 
+    FROM Authors A
+  """))
+  query_res = cursor
+  if query_res is None:
+    return redirect(employee_bp.url_prefix)
+  return render_template('employees/views/authors.html', authors=query_res)
+
+
+
+#All the edits
+@employee_bp.route('/add_publisher', methods=['GET'])
+def add_publisher_form():
+  return render_template('employees/edits/add_publisher.html')
+@employee_bp.route('/add_publisher', methods=['POST'])
+def add_publisher_update():
+  publisher_details = {
+    'publisher_name': request.form.get('publisher_name'),
+  }
+
+  cursor = g.conn.execute(text("""
+    INSERT INTO Publishers (publisher_name)
+    VALUES (:publisher_name);
+  """), publisher_details)
+
+  g.conn.commit()
+
+  return redirect(employee_bp.url_prefix + '/') 
+
+@employee_bp.route('/remove/customer', methods=['GET'])
+def get_customer():
+  return render_template('employees/edits/get_customer.html')
+
+@employee_bp.route('/remove/customer', methods=['POST'])
+def remove_customer():
+  if 'current_user_id' not in session or session['user_type'] != 'employee':
+    return redirect(employee_bp.url_prefix + '/login')
+  incorrect_message = "False"
+  sql_query_params = {
+    'user_id': request.form.get('customer_id')
+  } 
+  cursor = g.conn.execute(text("""
+    DELETE FROM Customers C 
+    WHERE C.user_id=:user_id
+  """), sql_query_params)
+  g.conn.commit()
+  affected_rows = cursor.rowcount
+  if affected_rows == 0:
+    incorrect_message = "True"
+  else:
+    incorrect_message = "False"
+  return render_template('employees/edits/get_customer.html', incorrect_details=incorrect_message)
